23d824304469e6493781b9dc26c812e8b420fbaa
pavadomino <pavadomino@gmail.com>
2023-07-17 00:40:06 +0200

    Update logger

--- app.rb
+++ app.rb
@@ -1,7 +1,21 @@
 class Logger
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
+  #class method
   def self.say_something
     puts 'Hey!'
+  end
+
+  #instance method
+  def log_something wat
+    @f.puts wat
   end
 end
 
 Logger.say_something
+
+logger = Logger.new
+logger.log_something 'hey!'
+
