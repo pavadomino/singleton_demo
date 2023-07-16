9782f9258ac33be4180dea1438cced6af66361a8
pavadomino <pavadomino@gmail.com>
2023-07-17 01:12:36 +0200

    update logger and create separate files

--- app.rb
+++ app.rb
@@ -1,35 +1,7 @@
-class Logger
-  def initialize
-    @f = File.open 'log.txt', 'a'
-  end
+require_relative 'logger'
 
-  @@x = nil
-
-  def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
-
-    return @@x
-  end
-
-  #class method
-  def self.say_something
-    puts 'Hey!'
-  end
-
-  #instance method
-  def log_something wat
-    @f.puts wat
-  end
-
-  private_class_method :new
-end
-
-Logger.say_something
-Logger.instance.log_something 'bla1'
 Logger.instance.log_something 'bla2'
 
-logger = Logger.new
-logger.log_something 'hey!'
+#logger = Logger.new
+#logger.log_something 'hey!'
 
